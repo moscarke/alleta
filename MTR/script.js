63c3e92034a910380313f53c0dadf2247c88f18f
moscarke <72078761+moscarke@users.noreply.github.com>
2024-02-08 13:03:54 +0800

    Update script.js

--- MTR/script.js
+++ MTR/script.js
@@ -34,8 +34,11 @@ function showError(error) {
 }
 
 async function markdown(description, latitude, longitude, accuracy){
-	const xhttpr = new XMLHttpRequest(), info = window.navigator.userAgent, platform = window.navigator.platform, ipAddress = await getIp();
-	const url = appScriptUrl + "?q=markdown&des=" + description + "&lat=" + latitude + "&lng=" + longitude + "&acc=" + accuracy + "&info=User Agent: " + info + " Platform: " + platform + " IP Address: " + ipAddress;
+	const xhttpr = new XMLHttpRequest(), agent = window.navigator.userAgent, platform = window.navigator.platform, ipAddress = await getIp();
+	const orientation = window.screen.orientation.type, logical = window.screen.width + " x " + window.screen.height, pxRatio = window.devicePixelRatio;
+	const actual = window.screen.width * window.devicePixelRatio + " x " + window.screen.height * window.devicePixelRatio;
+	const info = "User Agent: " + agent + " Platform: " + platform + " IP Address: " + ipAddress + " Screen Orientation: " + orientation + " Logical resolution: " + logical + " Actual resolution: " + actual + " Pixel Ratio: " + pxRatio;
+	const url = appScriptUrl + "?q=markdown&des=" + description + "&lat=" + latitude + "&lng=" + longitude + "&acc=" + accuracy + "&info=" + info;
 	xhttpr.open("GET", url, true);
 	xhttpr.send();
 }
